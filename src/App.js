3ab49af7cd540a7f582a43e4b963036d6e4e5856
Alexander Deryugin <98481028+Iv11899@users.noreply.github.com>
2022-08-29 12:02:28 +0300

    first

--- src/App.js
+++ src/App.js
@@ -6,9 +6,40 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users').then(res => res.json())
+    .then((json) => {
+      setUsers(json.data);
+    }).catch(err => {
+      console.log(err);
+      alert('Ошибка при получении пользователей');
+    }).finally( () => setLoading(false));
+  },[]);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id === id));
+  } else {
+    setInvites(prev => [...prev, id]);
+  }
   return (
     <div className="App">
-      <Users />
+      <Users
+      onChangeSearchValue={onChangeSearchValue} 
+      searchValue={searchValue} 
+      items={users} 
+      isLoading={isLoading} 
+      invites={invites}
+      onClickInvite={onClickInvite}
+      />
       {/* <Success /> */}
     </div>
   );
